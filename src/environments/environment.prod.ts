47a6d29a487ca3dfd806ede6591da7404c976484
twony26 <twonytwony@gmail.com>
2018-10-18 13:55:41 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCGMPI4kn0pWaN2to9i8-OR4HmGysRRB98",
-    authDomain: "porkeros-porkeras.firebaseapp.com",
-    databaseURL: "https://porkeros-porkeras.firebaseio.com",
-    projectId: "porkeros-porkeras",
-    storageBucket: "porkeros-porkeras.appspot.com",
-    messagingSenderId: "1001476931639"
+
   }
 };
